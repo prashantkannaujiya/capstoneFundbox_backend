92c25e260131f22533d1ebb11ff7bf04ff730892
prashantkannaujiya <prashantkannaujiya@gmail.com>
2024-10-20 18:40:39 +0530

    fix

--- Fundbox.js
+++ Fundbox.js
@@ -204,3 +204,4 @@ app.get('/search/:token',async(req,res)=>{
 
     const port=process.env.PORT || 2100;
     app.listen(port,()=>{console.log('server runnning on 2100')})
+    module.exports = app
